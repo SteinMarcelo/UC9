5f5098a2536f4dc0c5add30fc79503f5e2dcfe33
Marcelo Boeira <marcelosteinhaus.ms@gmail.com>
2024-06-06 19:39:03 -0300

    lista3completa

---
+++ AulaJavaMySql/src/main/java/com/mycompany/aulajavamysql/AulaJavaMySql.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aulajavamysql;
+
+/**
+ *
+ * @author Luiz Bataioli
+ */
+public class AulaJavaMySql {
+
+    public static void main(String[] args) {
+        
+        frmCadastroPessoa formCadastroPessoa = new frmCadastroPessoa();
+        formCadastroPessoa.setVisible(true);
+        
+        
+    }
+}
